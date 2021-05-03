b63128d75f79c388c5a88b0b12010ce6d0d27093
Evankhell561 <49106866+Evankhell561@users.noreply.github.com>
2021-05-03 16:15:10 -0400

    Update HoldersRoi.sol

--- contracts/completed_jobs/HoldersRoi.sol
+++ contracts/completed_jobs/HoldersRoi.sol
@@ -16,22 +16,22 @@ contract HoldersRoi is Pausable, Ownable {
 	mapping(address => User) public users;
 	uint256 public usersCount;
 
-	uint256 constant public WITHDRAW_COOLDOWN = 20 seconds; //weeks;
-	
+	uint256 constant public WITHDRAW_COOLDOWN = 20 seconds; //replace for: 1 weeks;
+
 	uint256 constant internal PERCENTS_DIVIDER = 10000;
-	uint256 constant internal BONUS_HOLD_TOKEN = 1; //0.01
-	uint256 constant internal MAX_USER_BONUS_DIVIDER = 500; //MAX: 5%
-	uint256 constant internal TIME_UPDATE = 10 seconds;
-	
+	uint256 constant internal BONUS_HOLD_TOKEN = 1; //0.01%
+	uint256 constant internal MAX_USER_BONUS = 500; //MAX: 5%
+	uint256 constant internal TIME_UPDATE = 10 seconds; //replace for: 1 days
+
 	uint256 public lastBalanceUpdate;
 	uint256 public baseReward;
 	uint256 public tokenStep;
-    uint256 public lowBalance;
+	uint256 public lowBalance;
 
 	event Newbie(address user);
 	event Withdrawn(address indexed user, uint256 amount);
 
-	constructor(IBEP20 token_) public  {
+	constructor(IBEP20 token_)  {
 		token = token_;
 		lastBalanceUpdate = block.timestamp;
 		tokenStep = 1000;
@@ -43,43 +43,53 @@ contract HoldersRoi is Pausable, Ownable {
 		_;
 	}
 
+	modifier isHolder_() {
+		require(isHolder(), "insufficient stake");
+		_;
+	}
+
+	function isHolder() internal view returns(bool) {
+		return (token.balanceOf(msg.sender) >= lowBalance);
+	}
+
 	modifier checkUser_() {
-	    User memory user_ =users[msg.sender];
-	    require(token.balanceOf(msg.sender) >= lowBalance, 'insufficient stake');
+		User memory user_ = users[msg.sender];
+		require(isHolder(), "insufficient stake");
 		require(isUser(user_) && (block.timestamp.sub(user_.checkpoint)).div(WITHDRAW_COOLDOWN) >= 1, "try again later");
 		_;
 	}
-	
+
 	function unpause() external onlyOwner returns(bool){
-	    _unpause();
-	    return true;
+		_unpause();
+		return true;
 	}
-	
+
 	function pause() external onlyOwner returns(bool){
-	    _pause();
-	    return true;
+		_pause();
+		return true;
 	}
-	
+
 	function isUser(User memory user_) internal pure returns(bool) {
 		return (user_.checkpoint > 0);
 	}
-	
+
 	// development phase only
-	function setusr(uint256 lowBalance_) external onlyOwner returns(bool) {
-	    usersCount = lowBalance_;
-	    return true;
-	}
-	
-	function setLowBalance(uint256 lowBalance_) external onlyOwner returns(bool) {
-	    lowBalance = lowBalance_;
-	    return true;
-	}
-	function setTokenStep(uint256 tokenStep_) external onlyOwner returns(bool) {
-	    tokenStep = tokenStep_;
-	    return true;
+	function setusr(uint256 newUsers) external onlyOwner returns(bool) {
+		usersCount = newUsers;
+		return true;
 	}
 
-	function register() external {
+	function setLowBalance(uint256 lowBalance_) external onlyOwner returns(bool) {
+		lowBalance = lowBalance_;
+		return true;
+	}
+
+	function setTokenStep(uint256 tokenStep_) external onlyOwner returns(bool) {
+		tokenStep = tokenStep_;
+		return true;
+	}
+
+	function register() external isHolder_ {
 		require(!isUser(users[msg.sender]), "user is already registered");
 		users[msg.sender].checkpoint = block.timestamp;
 		usersCount++;
@@ -92,25 +102,28 @@ contract HoldersRoi is Pausable, Ownable {
 
 	function getUserBonusPercent(address addr) public view returns (uint256) {
 		uint256 userBalance = token.balanceOf(addr).div(tokenStep).mul(BONUS_HOLD_TOKEN);
-		if(userBalance > MAX_USER_BONUS_DIVIDER)
-			userBalance = MAX_USER_BONUS_DIVIDER;
+		if(userBalance > MAX_USER_BONUS)
+			userBalance = MAX_USER_BONUS;
 		return userBalance;
 	}
 
-    function updatebaseReward() internal{
-        if(block.timestamp.sub(lastBalanceUpdate) >= TIME_UPDATE){
-            baseReward = getContractBalance().div(usersCount);
-            lastBalanceUpdate = block.timestamp;
-        }
-    }
-	function withdraw() external whenNotPaused contractHasfunds_ checkUser_  returns (bool) {
-	    updatebaseReward();
+	function updatebaseReward() internal {
+		if(block.timestamp.sub(lastBalanceUpdate) >= TIME_UPDATE){
+			uint256 _baseReward = getContractBalance().div(usersCount);
+			_baseReward = _baseReward.mul(PERCENTS_DIVIDER.sub(MAX_USER_BONUS)).div(PERCENTS_DIVIDER);
+			baseReward = _baseReward;
+			lastBalanceUpdate = block.timestamp;
+		}
+	}
+
+	function withdraw() external whenNotPaused contractHasfunds_ checkUser_ isHolder_ returns (bool) {
+		updatebaseReward();
 		uint256 contractBalance = getContractBalance();
-		uint256 returnPercent =getUserBonusPercent(msg.sender);
-		uint256 withdrawAmt = baseReward.add(contractBalance.mul(returnPercent).div(PERCENTS_DIVIDER));
+		uint256 returnPercent = getUserBonusPercent(msg.sender);
+		uint256 withdrawAmt = baseReward.add(baseReward.mul(returnPercent).div(PERCENTS_DIVIDER));
 		users[msg.sender].checkpoint = block.timestamp;
-		if( withdrawAmt > contractBalance)
-		    withdrawAmt = contractBalance;
+		if(withdrawAmt > contractBalance)
+			withdrawAmt = contractBalance;
 		token.transfer(msg.sender, withdrawAmt);
 		emit Withdrawn(msg.sender, withdrawAmt);
 		return true;
